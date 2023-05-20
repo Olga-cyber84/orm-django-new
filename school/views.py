be393a3944fb4392b01fd80f6ca5f4c09b9b732a
Olga-cyber84 <doorsdream007@ya.ru>
2023-05-20 12:05:38 +0300

    first commit

---
+++ school/views.py
@@ -0,0 +1,14 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    ordering = 'group'
+    object_list =Student.objects.order_by(ordering)
+    context = {'object_list':object_list}
+    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+
+    return render(request, template, context)
